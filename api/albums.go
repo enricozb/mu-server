0d83ab56e72c7162274864e876e8595a810b8fff
Enrico Borba <enricozb@gmail.com>
2021-08-03 19:25:25 -1000

    naive album cover support

--- api/albums.go
+++ api/albums.go
@@ -3,7 +3,10 @@ package api
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func (a *API) albums(w http.ResponseWriter, r *http.Request) {
@@ -12,4 +15,17 @@ func (a *API) albums(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
-func (a *API) albumCover(w http.ResponseWriter, r *http.Request) {}
+func (a *API) albumCover(w http.ResponseWriter, r *http.Request) {
+	album := mux.Vars(r)["id"]
+	data, err := a.lib.AlbumCover(album)
+
+	if err != nil {
+		http.Error(w, fmt.Errorf("album cover: %v", err).Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if _, err := io.Copy(w, data); err != nil {
+		http.Error(w, fmt.Errorf("copy: %v", err).Error(), http.StatusInternalServerError)
+		return
+	}
+}
--- library/albums.go
+++ library/albums.go
@@ -1,7 +1,30 @@
 package library
 
-import "github.com/enricozb/mu-server/metadata"
+import (
+	"fmt"
+	"io"
+
+	"github.com/enricozb/mu-server/metadata"
+)
 
 func (l *Library) Albums() map[string][]metadata.Metadata {
 	return l.albums
+}
+
+func (l *Library) AlbumCover(id string) (io.Reader, error) {
+	songs, ok := l.albums[id]
+	if !ok {
+		return nil, fmt.Errorf("album does not exist: %s", id)
+	}
+
+	if len(songs) == 0 {
+		return nil, fmt.Errorf("album is empty: %s", id)
+	}
+
+	path, err := l.Abs(songs[0].ID)
+	if err != nil {
+		return nil, fmt.Errorf("abs: %v", err)
+	}
+
+	return metadata.Cover(path)
 }
